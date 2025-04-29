8daf9b419677a7a6ae511cfae164e3b329d2cee6
ORIOLO GERALDINE <49374998@ort>
2025-04-29 08:35:17 -0300

    Update VTV.cs

--- Assets/VTV.cs
+++ Assets/VTV.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class VTV : MonoBehaviour
 {
+    public string PatenteDelVehiculo;
+    public int ModeloDeVehiculo;
+    public float KilometrosRecorridos;
+    public int vencimientoVTV;
+    public int HCppm;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        if (PatenteDelVehiculo != "")
+        {
+            Debug.Log(" Patente no puede estar vacia, VTV no aprobada");
+            return;
+        }
+        else if (1900 > ModeloDeVehiculo && 2025 < ModeloDeVehiculo)
+        {
+            Debug.Log(" El modelo de vehiculo no es valido, VTV no aprobada");
+            return;
+        }
+        else if (0 > KilometrosRecorridos)
+        {
+            Debug.Log("Los Kilometros no pueden ser negartivos, VTV no aprobada ");
+            return;
+        }
+        else if (2025 < vencimientoVTV && vencimientoVTV > ModeloDeVehiculo)
+        {
+            Debug.Log(" El vencimiento VTV no es valido, VTV no aprobada");
+            return;
+        } else ( 5 < HCppm){
+            Debug.Log("Los HC ppm deben de ser mayores a 5, VTV no aprobada");
+            return;
+        }
+            
+          
+       
     }
 
     // Update is called once per frame
